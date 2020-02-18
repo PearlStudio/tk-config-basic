09aba28b2297fd9b078c53e0f3ffc26170cc783f
JerryPearl <jerry.ma@pearlstudio.com>
2020-02-18 10:55:00 +0800

    rez init

---
+++ package.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+name = 'tk_config_basic'
+
+version = '1.3.1'
+
+description = 'Based on tk-config-basic'
+
+authors = ['']
+
+tools = []
+
+requires = []
+
+build_command = "python {root}/rezbuild.py {install}"
+
+
+def commands():
+    env.TK_CONFIG_BASIC_ROOT = "{root}"
+
+
+format_version = 2
